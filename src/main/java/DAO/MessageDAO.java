d9ed14d9751b0b577330607173a825ae03887c08
Steve Chilian <chilians@yahoo.com>
2025-04-02 08:26:14 +0000

    Updated message creation method.

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -21,7 +21,7 @@ public class MessageDAO {
                     return null;
                 }
             String sql = "INSERT INTO message (posted_by, message_text, time_posted_epoch) VALUES (?, ?, ?)";
-            PreparedStatement ps = connection.prepareStatement(sql);
+            PreparedStatement ps = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
             ps.setInt(1, message.getPosted_by());
             ps.setString(2, message.getMessage_text());
             ps.setLong(3, message.getTime_posted_epoch());
